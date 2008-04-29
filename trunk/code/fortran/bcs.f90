74e5f0d8c976848758662b412d5cc1984ba40ff2
dd46mi@mac.com <dd46mi@mac.com@c19052b8-f649-0410-84d6-b336a09adc77>
2008-04-29 14:53:54 +0000

    april, 29; today's fruitful work

--- trunk/code/fortran/bcs.f90
+++ trunk/code/fortran/bcs.f90
@@ -7,13 +7,13 @@ use nrtype
 use nr, only: midinf,qromo
 
 INTERFACE
-FUNCTION integrand(x,v,temp,epsilon)
-use nrtype
-REAL(SP),PARAMETER :: e=1.602176487E-19,k=8.617343E-5
-REAL(SP), DIMENSION(:), INTENT(IN) :: x
-REAL(SP), DIMENSION(size(x)) :: integrand
-REAL(SP) :: v,epsilon,temp
-END FUNCTION integrand
+	FUNCTION integrand(x,v,temp,epsilon)
+	use nrtype
+	REAL(SP),PARAMETER :: e=1.602176487E-19,k=8.617343E-5 !Units of k --> eV/K
+	REAL(SP), DIMENSION(:), INTENT(IN) :: x
+	REAL(SP), DIMENSION(size(x)) :: integrand
+	REAL(SP) :: v,epsilon,temp
+	END FUNCTION integrand
 END INTERFACE
 
 REAL(SP), DIMENSION(:,:), INTENT(OUT) :: didparam
@@ -24,64 +24,46 @@ REAL(SP) :: ia,ib
 REAL(SP),DIMENSION(:) :: param
 REAL(SP),DIMENSION(:) :: v
 REAL(SP),DIMENSION(:),intent(out) :: int
-!do n = 1,10000
-!print*,n,integrand((/ 1.0/(n*n) , 2.0 /))
 
-!end do
-!pause
 
-!print*,"los parametros en el bcs son:",param
-epsilon = param(1)
-temp = param(2)
-c = param(3)
+epsilon = param(1)	!Gap
+temp = param(2)		!Temperature
+c = param(3)		!Normal-Normal Conductance
 
-!print*,epsilon,temp,c
+print*,"Tentative parameters: epsilon, temperature and normal-normal conductance:", param
 
-!open(unit=1,file="iv_teorico.txt",status="replace",action="write",position="rewind")
-
-int=0.0
-
-print*,"param",param
+ia=k*temp
+ib=1.0
+!ib=maxval(abs(v))+2*epsilon !Porque se ve que a partir de ese valor la funcion es asintoticamente nula
 do n=1,size(v)
-!v=real(n)/puntos*vmax
-   !print*,k*temp-epsilon+v(n),k*temp-epsilon-v(n)
-   ia=1e-6
-   ib=maxval(k*temp-epsilon+v)*10
    int(n) = qromo(integrand,ia,ib,midinf,v(n),temp,epsilon)
-   int(n) = int(n) + sqrt(ia*(2*epsilon+ia))*(1/(1+exp((epsilon-v(n))/(k*temp)))-1/(1+exp((epsilon+v(n))/(k*temp))))
-   !print*,sqrt(ia*(2*epsilon+ia))*(1/(1+exp((epsilon-v(n))/(k*temp)))-1/(1+exp((epsilon+v(n))/(k*temp))))/int(n)
+   
+   !Ahora le sumo la parte que falta, de 0 hasta ia, que lo saco analiticamente...
+   !int(n) = int(n) + sqrt(ia*(2*epsilon+ia))*(1/(1+exp((epsilon-v(n))/(k*temp)))-1/(1+exp((epsilon+v(n))/(k*temp))))
+   
    didparam(n,1) =  - (int(n) -  qromo(integrand,ia,ib,midinf,v(n),temp,epsilon+hEpsilon))/hEpsilon
-   didparam(n,1) = didparam(n,1)+sqrt(ia*(2*epsilon+hEpsilon+ia))*(1/(1+exp((epsilon+hEpsilon-v(n))/(k*temp))) &
-   & -1/(1+exp((epsilon+hEpsilon+v(n))/(k*temp))))/hEpsilon
+   !didparam(n,1) = didparam(n,1)+sqrt(ia*(2*epsilon+hEpsilon+ia))*(1/(1+exp((epsilon+hEpsilon-v(n))/(k*temp))) &
+   !& -1/(1+exp((epsilon+hEpsilon+v(n))/(k*temp))))/hEpsilon
+   
    didparam(n,2) = - (int(n) -  qromo(integrand,ia,ib,midinf,v(n),temp+hTemp,epsilon))/hTemp 
-   didparam(n,2) = didparam(n,2) + sqrt(ia*(2*epsilon+ia))*(1/(1+exp((epsilon-v(n))/(k*(temp+hTemp)))) &
-   &-1/(1+exp((epsilon+v(n))/(k*(temp+hTemp)))))/hTemp
+   !didparam(n,2) = didparam(n,2) + sqrt(ia*(2*epsilon+ia))*(1/(1+exp((epsilon-v(n))/(k*(temp+hTemp)))) &
+   !&-1/(1+exp((epsilon+v(n))/(k*(temp+hTemp)))))/hTemp
+   
    didparam(n,3) = 0.0
-   !ia=1e-4
-   !ib=1.0
-   !int(n) = int(n) + qromo(integrand,ia,ib,midinf,v(n),temp,epsilon)
-   !ia=1.0
-   !ib=1e20
-   !int(n) = int(n) + qromo(integrand,ia,ib,midinf,v(n),temp,epsilon)   
-	!write(unit=1,fmt=*) v,i
-	!print*,n,int(n),v(n)
 end do
 
-
-
-
-!didparam=1.0
-   
-   
-!close(unit=1,status="keep")
 didparam = c*didparam
 didparam(:,3) = int
 int=c*int
-!print*,int
-!print*,"didparam",didparam
-!print*,"size(int) en bcs",size(int),int
 
-!-------------------
+open(unit=1,file="iv_teorico.txt",status="replace",action="write",position="rewind")
+	do n=1,size(v)
+	   write(unit=1,fmt=*) v(n),int(n)
+	end	do
+close(unit=1,status="keep")
+
+!--------------------------------------------
 END SUBROUTINE BCS
-
+!--------------------------------------------
+!--------------------------------------------
 end module m_bcs
--- trunk/code/fortran/m_bcs.mod
+++ trunk/code/fortran/m_bcs.mod
@@ -1,6 +1,6 @@
-G95 module created on Mon Apr 28 21:16:31 2008 from bcs.f90
+G95 module created on Tue Apr 29 13:46:15 2008 from bcs.f90
 If you edit this, you'll get what you deserve.
-
+module-version 6
 (() () () () () () () () () () () () () () () () () () () () ())
 
 ()
@@ -11,18 +11,18 @@ If you edit this, you'll get what you deserve.
 
 ()
 
-(2 'm_bcs' 'm_bcs' 1 ((MODULE UNKNOWN UNKNOWN UNKNOWN NONE) (UNKNOWN 0 ())
-0 0 () () '' () ())
-3 'bcs' 'm_bcs' 1 ((PROCEDURE UNKNOWN MODULE-PROC DECL NONE SUBROUTINE)
-(PROCEDURE 0 ()) 0 0 (4 NONE 5 NONE 6 NONE 7 NONE) () '' () ())
-5 'param' '' 8 ((VARIABLE UNKNOWN UNKNOWN UNKNOWN NONE DIMENSION DUMMY)
-(REAL 4 ()) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
-4 'v' '' 8 ((VARIABLE UNKNOWN UNKNOWN UNKNOWN NONE DIMENSION DUMMY) (
-REAL 4 ()) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
-7 'didparam' '' 8 ((VARIABLE OUT UNKNOWN UNKNOWN NONE DIMENSION DUMMY) (
-REAL 4 ()) 0 0 () (2 ASSUMED_SHAPE () () () ()) '' () ())
-6 'int' '' 8 ((VARIABLE OUT UNKNOWN UNKNOWN NONE DIMENSION DUMMY) (REAL
-4 ()) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
+(2 'bcs' 'm_bcs' 1 ((PROCEDURE UNKNOWN MODULE-PROC DECL NONE NONE
+SUBROUTINE) (PROCEDURE 0) 0 0 (3 NONE 4 NONE 5 NONE 6 NONE) () '' () ())
+7 'm_bcs' 'm_bcs' 1 ((MODULE UNKNOWN UNKNOWN UNKNOWN NONE NONE) (
+UNKNOWN) 0 0 () () '' () ())
+6 'didparam' '' 8 ((VARIABLE OUT UNKNOWN UNKNOWN NONE NONE DIMENSION
+DUMMY) (REAL 4) 0 0 () (2 ASSUMED_SHAPE () () () ()) '' () ())
+5 'int' '' 8 ((VARIABLE OUT UNKNOWN UNKNOWN NONE NONE DIMENSION DUMMY) (
+REAL 4) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
+4 'param' '' 8 ((VARIABLE UNKNOWN UNKNOWN UNKNOWN NONE NONE DIMENSION
+DUMMY) (REAL 4) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
+3 'v' '' 8 ((VARIABLE UNKNOWN UNKNOWN UNKNOWN NONE NONE DIMENSION DUMMY)
+(REAL 4) 0 0 () (1 ASSUMED_SHAPE () ()) '' () ())
 )
 
-('bcs' 0 3 'm_bcs' 0 2)
+('bcs' 0 2 'm_bcs' 0 7)
